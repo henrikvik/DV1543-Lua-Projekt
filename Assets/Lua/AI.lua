c318d4e229c75d36c37db2f547684723fed8a22e
HannahLovberg <hannah.lovberg@live.com>
2017-05-15 11:46:27 +0200

    AI

--- Assets/Lua/AI.lua
+++ Assets/Lua/AI.lua
@@ -30,3 +30,29 @@ function onCollision(otherRadius, distance)
 		this.radius = this.radius - difference;
 	end
 end
+
+function findFood(foodList, nrOfFood)	
+
+	index = 1
+
+	for i = 1, nrOfFood, 1 do		-- syntax?  for i = 1, loop nrOfFood times, plus 1 in every loop?
+
+		if(foodList[i].position > foodList[min])
+			min = i
+		end
+	end
+
+	return foodList[min].position  
+end
+
+function moveTo(newPos, oldPos, delta)	
+
+	dir = normalize(newPos- oldPos)
+
+	self.Move(
+	dir.x * delta,
+	dir.y * delta
+	);	
+
+end
+
