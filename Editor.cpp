a6afb5f62a5eb6a619d8e541b7c2e2f5377020cc
HannahLovberg <hannah.lovberg@live.com>
2017-05-24 20:52:08 +0200

    editor

--- Editor.cpp
+++ Editor.cpp
@@ -12,7 +12,19 @@ std::vector<Blob*> Editor::readFromFile()
 	return blobs;
 }
 
-void Editor::writeToFile()
+void Editor::writeToFile(float hm, float growthRate, float redColor, float greenColor, float blueColor, const char name, const char script)
 {
+	std::ofstream myFile;
+	myFile.open("Editor.txt");
 
+	myFile	<< hm << " " 
+			<< growthRate << " " 
+			<< redColor << " " 
+			<< greenColor << " " 
+			<< blueColor << " " 
+			<< name << " " 
+			<< script 
+			<< std::endl;
+
+	myFile.close();
 }
--- Editor.h
+++ Editor.h
@@ -10,7 +10,7 @@ class Editor
 public:
 
 	std::vector<Blob*> readFromFile();
-	void writeToFile();
+	void writeToFile(float hm, float growthRate, float redColor, float greenColor, float blueColor, const char name, const char script);
 
 private:
 
