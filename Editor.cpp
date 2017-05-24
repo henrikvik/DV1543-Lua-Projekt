75d0ef2ad98a1895fbbd8764bf092e9618389ce4
HannahLovberg <hannah.lovberg@live.com>
2017-05-24 20:40:36 +0200

    Started Editor

---
+++ Editor.cpp
@@ -0,0 +1,18 @@
+#include "Editor.h"
+
+std::vector<Blob*> Editor::readFromFile()
+{
+	std::vector<Blob*> blobs;
+	
+	float radius;
+	float growthRate;
+
+
+
+	return blobs;
+}
+
+void Editor::writeToFile()
+{
+
+}
---
+++ Editor.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "Source\Blob.h"
+
+
+
+class Editor
+{
+public:
+
+	std::vector<Blob*> readFromFile();
+	void writeToFile();
+
+private:
+
+
+};
